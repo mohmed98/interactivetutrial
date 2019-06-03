28652efb69140e6a7da3c07dbac7622dd15bd11f
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2019-06-03 04:48:57 +0200

    push

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,62 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             title: Text('flutter interactive tutrrial'),
-            ),
-            body: Center(
-            child: TapboxA(),
-        ),
-        )
+          ),
+          body: Center(
+            child: ParentWidget(),
+          ),
+        ));
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newVlue) {
+    setState(() {
+      _active = newVlue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
     );
   }
 }
 
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
-
-  @override
-  _TapboxAState  createState() => _TapboxAState();
-}
-
-class _TapboxAState  extends State<TapboxA> {
-  bool _active = false;
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
   void _handleTap() {
-    setState(() {
-      _active  = !_active;
-    });
+    onChanged(!active);
   }
+
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _handleTap,
       child: Container(
         child: Center(
           child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
+            active ? 'Avtive' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.black),
           ),
         ),
         width: 200.0,
-          height: 200.0,
+        height: 200.0,
         decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
       ),
     );
   }
